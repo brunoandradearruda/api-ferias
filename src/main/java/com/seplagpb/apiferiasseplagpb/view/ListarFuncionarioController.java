5ff7e82cd4bc72b5bd4fa40f05ef46198672ba87
“bruno <brunoandradearruda@gmail.com>
2024-02-23 16:26:10 -0300

    página cadastro de funcionário criada. controllerView também criado

---
+++ src/main/java/com/seplagpb/apiferiasseplagpb/view/ListarFuncionarioController.java
@@ -0,0 +1,24 @@
+package com.seplagpb.apiferiasseplagpb.view;
+import com.seplagpb.apiferiasseplagpb.service.FuncionarioService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ListarFuncionarioController {
+
+    private final FuncionarioService funcionarioService;
+
+    public ListarFuncionarioController(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
+
+    @GetMapping("/funcionarios/listar")
+    public String listarFuncionarios(Model model) {
+        // Obtém a lista de funcionários do serviço
+        model.addAttribute("funcionarios", funcionarioService.listarFuncionariosCadastrados());
+
+        // Retorna o nome da página Thymeleaf que exibirá a lista de funcionários
+        return "funcionario/listar";
+    }
+}
