ffb58b5d3d3c521f8a674374781bb26e2e64840e
“bruno <brunoandradearruda@gmail.com>
2024-03-08 21:36:43 -0300

    registro de férias- OK

--- src/main/java/com/seplagpb/apiferiasseplagpb/controller/UnidadeDeTrabalhoController.java
+++ src/main/java/com/seplagpb/apiferiasseplagpb/controller/UnidadeDeTrabalhoController.java
@@ -13,44 +13,7 @@ public class UnidadeDeTrabalhoController {
 
     @Autowired
     private UnidadeDeTrabalhoService unidadeDeTrabalhoService;
-
-    // Listar unidades existentes
-//    @GetMapping
-//    public String listarUnidades(Model model) {
-//        model.addAttribute("unidades", unidadeDeTrabalhoService.listarTodos());
-//        return "unidades";
-//    }
-
-    // Mostrar formulário para adicionar uma nova unidade
-//    @GetMapping("/nova")
-//    public String mostrarFormularioDeNovaUnidade(Model model) {
-//        model.addAttribute("unidade", new UnidadeDeTrabalho());
-//        return "unidade-formulario";
-//    }
-
-//    // Processar o formulário de nova unidade
-//    @PostMapping("/salvar")
-//    public String salvarUnidade(@ModelAttribute("unidade") UnidadeDeTrabalho unidade) {
-//        unidadeDeTrabalhoService.salvarOuAtualizar(unidade);
-//        return "redirect:/unidades";
-//    }
-
-
-//    // Mostrar formulário para editar uma unidade existente
-//    @GetMapping("/editar/{id}")
-//    public String mostrarFormularioDeEdicao(@PathVariable Long id, Model model) {
-//        UnidadeDeTrabalho unidade = unidadeDeTrabalhoService.encontrarPorId(id)
-//                .orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
-//        model.addAttribute("unidade", unidade);
-//        return "unidade-formulario";
-//    }
-
-//    // Excluir uma unidade
-//    @GetMapping("/excluir/{id}")
-//    public String excluirUnidade(@PathVariable Long id) {
-//        unidadeDeTrabalhoService.excluir(id);
-//        return "redirect:/unidades";
-//    }
+    
 
 
 }
