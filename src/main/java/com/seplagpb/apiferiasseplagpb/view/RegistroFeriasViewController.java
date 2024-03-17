28994861955184e1352231ae853ec9fdbfd3bc82
“bruno <brunoandradearruda@gmail.com>
2024-03-17 13:13:55 -0300

    Cadastro de Cargos - OK

--- src/main/java/com/seplagpb/apiferiasseplagpb/view/RegistroFeriasViewController.java
+++ src/main/java/com/seplagpb/apiferiasseplagpb/view/RegistroFeriasViewController.java
@@ -1,5 +1,5 @@
 package com.seplagpb.apiferiasseplagpb.view;
-
+import org.springframework.web.multipart.MultipartFile;
 import com.seplagpb.apiferiasseplagpb.dto.RegistrarFeriasRequest;
 import com.seplagpb.apiferiasseplagpb.model.Funcionario;
 import com.seplagpb.apiferiasseplagpb.service.FuncionarioService;
@@ -10,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
